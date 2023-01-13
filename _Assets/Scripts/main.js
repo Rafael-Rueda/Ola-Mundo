c2c923861a472bde7fd512d41eea39c1c87ebba1
Rafa <rafael.rued4@gmail.com>
2023-01-13 17:23:12 -0300

    commit 1

---
+++ _Assets/Scripts/main.js
@@ -0,0 +1,5 @@
+import { header_style } from "./styles.js";
+
+
+window.addEventListener('load', header_style);
+window.addEventListener('resize', header_style)
---
+++ _Assets/Scripts/styles.js
@@ -0,0 +1,14 @@
+export function header_style() {
+    const $div = document.querySelector('.header-content > div');
+    if (window.innerWidth < 950) {
+        $div.children[1].innerHTML = '<a href="https://github.com/Rafael-Rueda" target="_blank"> => Linkedin</a>'
+        $div.children[2].innerHTML = '<a href="https://github.com/Rafael-Rueda" target="_blank"> => Github</a>'
+    } else if (window.innerWidth < 1400) {
+        $div.children[1].innerHTML = ' => Linkedin: <a href="https://www.linkedin.com/in/rafael-rueda-03477a237/" target="_blank">https://www.linkedin.com/in/rafael-rueda-03477a237/</a>'
+        $div.children[2].innerHTML = ' => Github: <a href="https://github.com/Rafael-Rueda" target="_blank">https://github.com/Rafael-Rueda</a>'
+    } else {
+        $div.children[1].innerHTML = '<a href="https://github.com/Rafael-Rueda" target="_blank"> => Linkedin</a>'
+        $div.children[2].innerHTML = '<a href="https://github.com/Rafael-Rueda" target="_blank"> => Github</a>'
+    }
+    
+}
